b7a1b094895a5a3f461f5be5eb95c7db6699a9cb
Kenny Le <kle34@huskers.unl.edu>
2024-08-26 22:33:58 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+int main(int argc, char **argv) {
+
+    printf("Hello, I'm Kenny Le!\n");
+    printf(Major is Computer Engineering!\n");
+
+    return 0;
+}
