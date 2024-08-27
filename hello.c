94f1ad68362c277f7386dbeeb09123cb2f31b714
Kenny Le <kle34@huskers.unl.edu>
2024-08-26 22:54:54 -0500

    fixed standard library bug

--- hello.c
+++ hello.c
@@ -1,11 +1,13 @@
 /**
  * Basic Hello World Program
  */
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
 
     printf("Hello, I'm Kenny Le!\n");
-    printf(Major is Computer Engineering!\n");
+    printf("Major is Computer Engineering!\n");
 
     return 0;
 }
